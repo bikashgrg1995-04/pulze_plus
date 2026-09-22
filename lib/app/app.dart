7f6828df91a6e3fa55ba58da7854f8f7d2a8bf47
Bikash Gurung <bikashgurung.official@gmail.com>
2026-09-22 13:22:23 +0545

    Initial Pulze+ app setup

---
+++ lib/app/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PulzeApp extends StatelessWidget {
+  const PulzeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pulze+',
+      debugShowCheckedModeBanner: false,
+      home: SizedBox(),
+    );
+  }
+}
